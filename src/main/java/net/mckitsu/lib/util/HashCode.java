368776d7e19592c940e978502d687bdbc8bb57ca
ZxyKira <danny41553@gmail.com>
2020-12-06 18:33:50 +0800

    [add] add HashCode
    [add] add HashCode
    
    support HashCode type MD2 MD5 SHA1 SHA-256 SHA-384 SHA-512

---
+++ src/main/java/net/mckitsu/lib/util/HashCode.java
@@ -0,0 +1,31 @@
+package net.mckitsu.lib.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class HashCode {
+    private final MessageDigest messageDigest;
+
+    public HashCode(HashType type) throws NoSuchAlgorithmException {
+        messageDigest = MessageDigest.getInstance(type.type);
+    }
+
+    public byte[] hash(byte[] input){
+        return this.messageDigest.digest(input);
+    }
+
+    public enum HashType{
+        MD2("MD2"),
+        MD5("MD5"),
+        SHA1("SHA1"),
+        SHA256("SHA-256"),
+        SHA384("SHA-384"),
+        SHA512("SHA-512");
+
+        HashType(String type){
+            this.type = type;
+        }
+
+        private final String type;
+    }
+}
