7d9bee77d3d30fc372b256da3096dc225cc71d87
ZxyKira <danny41553@gmail.com>
2021-01-01 12:17:26 +0800

    [modify] make EventHandle construct public
    [modify] make EventHandle construct public

--- src/main/java/net/mckitsu/lib/util/EventHandler.java
+++ src/main/java/net/mckitsu/lib/util/EventHandler.java
@@ -11,7 +11,7 @@ public abstract class EventHandler {
     /* **************************************************************************************
      *  Construct method
      */
-    protected EventHandler(){}
+    public EventHandler(){}
 
     /* **************************************************************************************
      *  Override method
